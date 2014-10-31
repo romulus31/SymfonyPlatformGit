1b9070f3dc552e20a20b9dd809aef258bbbe7279
fauconnierr <fauconnierr@FR-D23367.groupinfra.com>
2014-10-31 17:00:01 +0100

    Commit origin/master
    	README.md	Modified	Commit	README.md
    	routing.yml	Modified	Commit	app\config\routing.yml
    	AdvertController.php	Modified	Commit	src\RMS\PlatformBundle\Controller\AdvertController.php
    	routing.yml	Modified	Commit	src\RMS\PlatformBundle\Resources\config\routing.yml

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ SymfonyPlatformGit
 ==================
 
 A great website to make adverts with the famous Framework Symfony 2
+
+Préfixe rajouté dans le app/config/routing.yml : advert
--- src/RMS/PlatformBundle/Controller/AdvertController.php
+++ src/RMS/PlatformBundle/Controller/AdvertController.php
@@ -15,12 +15,14 @@ class AdvertController extends Controller
 {
     public function indexAction()
     {
-        $content = $this->get('templating')->render('RMSPlatformBundle:Advert:advert.html.twig');
-        return new Response($content);
+       $content = $this->get('templating')->render('RMSPlatformBundle:Advert:advert.html.twig');
+       return new Response($content);
+        
     }
     
     public function viewAction($id){
-        $content = $this->get('templating')->render('RMSPlatformBundle:Advert:view.html.twig');
-        return new Response($content);
+        //$content = $this->get('templating')->render('RMSPlatformBundle:Advert:view.html.twig');
+        //return new Response($content);
+        return new Response("id de la vue : ".$id);
     }
 }
